1c0ca020fff0223dc1a5f0c49410788711ea847f
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2022-09-21 09:43:07 +0200

    Add project files.

---
+++ Kuldetes.cs
@@ -0,0 +1,29 @@
+﻿namespace CA220921
+{
+    internal class Kuldetes
+    {
+        //prop + tab + tab
+        public string Kod { get; set; }
+        public DateOnly KilovesDatuma { get; set; }
+        public string SikloNeve { get; set; }
+        private int KuldetesNap { get; set; }
+        private int KuldetesOra { get; set; }
+        public int KuldetesHossza => KuldetesNap * 24 + KuldetesOra;
+        public string LandolasHelye { get; set; }
+        public int LegenysegSzama { get; set; }
+
+        //ctor + tab + tab
+        public Kuldetes(string fileEgySora)
+        {
+            string[] v = fileEgySora.Split(';');
+
+            Kod = v[0];
+            KilovesDatuma = DateOnly.Parse(v[1]);
+            SikloNeve = v[2];
+            KuldetesNap = int.Parse(v[3]);
+            KuldetesOra = int.Parse(v[4]);
+            LandolasHelye = v[5];
+            LegenysegSzama = int.Parse(v[6]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using CA220921;
+
+List<Kuldetes> kuldetesek = new();
+
+using StreamReader sr = new(@"..\..\..\res\kuldetesek.csv");
+while (!sr.EndOfStream) kuldetesek.Add(new Kuldetes(sr.ReadLine()));
+
+//3. feladat
+Console.WriteLine(
+    $"3.feladat:\n\t" +
+    $"Összesen {kuldetesek.Count} alkalommal indítottak űrsiklót.");
+
+//4. feladat
+
